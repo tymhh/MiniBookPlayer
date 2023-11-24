baf10ba099b102b27699d7ccdd7d94f8046d704f
tymh <tim.hazhyi@gmail.com>
2023-11-24 14:21:41 +0100

    Initial Commit

---
+++ MiniBookListener/MiniBookListenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBookListenerApp.swift
+//  MiniBookListener
+//
+//  Created by Tim Hazhyi on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniBookListenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
